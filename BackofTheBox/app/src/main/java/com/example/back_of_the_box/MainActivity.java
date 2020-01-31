d513322540d01fbd916e18f3857674425577db37
WitheyFish <32938905+WitheyFish@users.noreply.github.com>
2020-01-31 13:45:31 -0800

    Add files via upload

---
+++ BackofTheBox/app/src/main/java/com/example/back_of_the_box/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.back_of_the_box;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
